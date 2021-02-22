ea3d611a3b270f3e055ef606cce1b6077f0525b6
Ines L <inesluparia@gmail.com>
2021-02-22 22:13:56 +0100

    først del af opgaven

---
+++ src/main/java/com/example/demo/Services/Post.java
@@ -0,0 +1,23 @@
+package com.example.demo.Services;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Post {
+
+    public String title;
+    public String content;
+    public String private_public;
+    public String stringDate;
+    //LocalDate date;
+
+    public static ArrayList<Post> posts = new ArrayList<>();
+
+    public Post(String title, String content, String private_public, String date){
+        this.title=title;
+        this.content= content;
+        this.private_public= private_public;
+        stringDate = date;
+        //this.date=LocalDate.parse(date);
+    }
+}
