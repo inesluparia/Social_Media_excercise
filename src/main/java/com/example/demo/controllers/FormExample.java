36ea0994fc77da6ead3100674276622edbfa0397
Ines L <inesluparia@gmail.com>
2021-03-01 10:54:20 +0100

    added views to last endpoints

--- src/main/java/com/example/demo/controllers/FormExample.java
+++ src/main/java/com/example/demo/controllers/FormExample.java
@@ -13,12 +13,17 @@ import static com.example.demo.Model.Post.posts;
 @Controller
 public class FormExample {
 
-    @GetMapping(value = "/form")
-    public String renderForm() {
-        return "form";
+    @GetMapping("/")
+    public String home(){
+            return "index.html";
     }
 
-    @PostMapping("/create-post")
+    @GetMapping(value = "/submit")
+    public String renderForm() {
+        return "form.html";
+    }
+
+    @PostMapping("/submit-post")
     public String createNewPost(@RequestParam("title") String title, @RequestParam ("post_content") String content,
                                @RequestParam ("private-public") String pp, @RequestParam ("date") String date){
         Post post = new Post(title,content,pp,date);
@@ -27,20 +32,19 @@ public class FormExample {
     }
 
     @GetMapping("/success")
-    @ResponseBody
     public String newPostSuccess(Model successMdl){
         int index = Post.posts.size()-1;
         Post post = Post.posts.get(index);
-        successMdl.addAttribute("YourPost",post);
-        return "Your post has been created\n"+post.title;
+        successMdl.addAttribute("yourPost",post);
+        return "success.html";
+        //return "Your post has been created\n"+post.title;
     }
 
     @GetMapping("/dashboard")
     public String dashboard(Model model){
         ArrayList<Post>publicPosts = new ArrayList<>();
-        publicPosts.add(new Post("Title","this is the shit","Public","some date"));
-        publicPosts.add(new Post("Title 2","I have so much to share with the world. cjiocre aeriv cjioac o cioa c ocai ifd fisdo vcafe.","Public","22/07/14"));
-        publicPosts.add(new Post("3","this is the shit","Public","22/05/17"));
+        publicPosts.add(new Post("Title 1","I have so much to share with the world. cjiocre aeriv cjioac o cioa c ocai ifd fisdo vcafe.","Public","22/07/14"));
+        publicPosts.add(new Post(" Some title 2","this is the shit","Public","22/05/17"));
         for (Post post : posts){
             if (post.private_public.equals("Public")){
                 publicPosts.add(post);
